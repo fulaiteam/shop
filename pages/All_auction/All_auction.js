5621b7bf47f520a4b65f3f7e46d58696f6e0392e
oil <549988340@qq.com>
2021-01-13 16:28:02 +0800

    增加页面

---
+++ pages/All_auction/All_auction.js
@@ -0,0 +1,119 @@
+// pages/All_auction/All_auction.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+name:'1234444',
+day:'55',
+show:true,
+isshow:false,
+array:[  //拍卖宝贝的数组
+{
+  img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/chanpin.png',
+  title:'AGV k1摩特车头盔',
+  price:'¥620.80',
+  time:'00:20:10'
+},
+{
+  img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/chanpin.png',
+  title:'AGV k1摩特车头盔',
+  price:'¥620.80',
+  time:'00:20:10'
+},
+{
+  img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/chanpin.png',
+  title:'AGV k1摩特车头盔',
+  price:'¥620.80',
+  time:'00:20:10'
+},
+{
+  img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/chanpin.png',
+  title:'AGV k1摩特车头盔',
+  price:'¥620.80',
+  time:'00:20:10'
+},
+{
+  img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/chanpin.png',
+  title:'AGV k1摩特车头盔',
+  price:'¥620.80',
+  time:'00:20:10'
+},
+{
+  img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/chanpin.png',
+  title:'AGV k1摩特车头盔',
+  price:'¥620.80',
+  time:'00:20:10'
+},
+
+]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+show:function() {
+  this.setData({
+    show : true,
+    isshow:false
+  })
+},
+isshow:function() {
+  this.setData({
+    isshow : true,
+    show:false
+  })
+},
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- pages/Sale/Sale.js
+++ pages/Sale/Sale.js
@@ -2,6 +2,16 @@
 const app = getApp();
 Page({
   data: {
+    leftMin: 0,    //价格的进度条
+    leftMax: 6,    //价格的进度条
+    rightMin: 0,    //价格的进度条
+    rightMax: 6,    //价格的进度条
+    leftValue: 0,    //价格的进度条
+    rightValue: 6,    //价格的进度条
+    select:false,   //类型下拉隐藏
+    jiageselect:false,   //价格下拉隐藏
+    jiage:'价格排序 >  ',
+  chanpin:'产品类型 >',
     indicatorDots: true,    //轮播图的点
     btns:["全部","机车","头盔","配件","改装保养"],
     cons: ["dataList", "RP", "AE", "C4D"],
@@ -16,12 +26,13 @@ Page({
     goods_img:'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
     goods_xiaoliang:'限量抢购',
     goods_price:'售价： ¥59,000',     //打折后
-    goods_original_price: '¥1750.00'   //原价
+    goods_original_price: '¥1750.00',   //原价
+    img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/ysc.png',
   },
   {
     goods_name:'全部', 
     isActive: false,
-
+img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/sc.png',
     goods_id:1,
     goods_title:'重庆路宝宝奔雷名车1314646 11635',
     goods_text:'男女四季全盔街车跑车冬季',
@@ -29,7 +40,7 @@ Page({
     goods_img:'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
     goods_xiaoliang:'限量抢购',
     goods_price:'售价： ¥59,000',
-    goods_original_price: '¥1750.00'   //原价
+    goods_original_price: '¥1750.00' ,  //原价
 
 
   }, {
@@ -40,7 +51,7 @@ Page({
     goods_title: '重庆路宝宝奔雷名车1314646 11635',
     goods_text:'男女四季全盔街车跑车冬季',
     goods_original_price: '¥1750.00',   //原价
-
+img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/sc.png',
 
     goods_img: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
     goods_xiaoliang: '限量抢购',
@@ -48,7 +59,7 @@ Page({
   }, {
     goods_name:'全部', 
     isActive: false,
-
+img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/sc.png',
     goods_id: 1,
     goods_title: '重庆路宝宝奔雷名车1314646 11635',
     goods_text:'男女四季全盔街车跑车冬季',
@@ -61,7 +72,7 @@ Page({
   }, {
     goods_name:'全部', 
     isActive: false,
-
+img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/sc.png',
     goods_id: 1,
     goods_title: '重庆路宝宝奔雷名车1314646 11635',
     goods_text:'男女四季全盔街车跑车冬季',
@@ -76,6 +87,33 @@ Page({
 
 },
 
+bindjiage() {
+  this.setData({
+    jiageselect:!this.data.jiageselect
+  })
+},
+jiageselect(e) {
+  console.log(e.currentTarget.dataset.name);
+  var name = e.currentTarget.dataset.name
+  this.setData({
+    jiage:name,
+    jiageselect:false
+  })
+},
+bindshow() {
+  this.setData({
+    select:!this.data.select
+  })
+},
+
+myselect(e) {
+
+  var name = e.currentTarget.dataset.name
+  this.setData({
+    chanpin:name,
+    select:false
+  })
+},
   //获取输入框的内容
   getInputContent:function(e){
     this.setData({
