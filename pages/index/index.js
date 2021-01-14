f3b8dfa13c3ec53cd98bcd9f0b34b32e447dcbae
wenmengqi <574986464@qq.com>
2021-01-14 18:43:52 +0800

    首页完善

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,23 @@
 const app = getApp();
 Page({
   data: {
+    leftMin: 0, //价格的进度条
+    leftMax: 6, //价格的进度条
+    rightMin: 0, //价格的进度条
+    rightMax: 6, //价格的进度条
+    leftValue: 0, //价格的进度条
+    rightValue: 6, //价格的进度条
+    select: false, //类型下拉隐藏
+    jiageselect: false, //价格下拉隐藏
+    jiage: '价格排序 >  ',
+    chanpin: '产品类型 >',
+
     indicatorDots: true, //轮播图的点
     btns: ["全部", "机车", "头盔", "配件", "改装保养"],
     cons: ["dataList", "RP", "AE", "C4D"],
     active: 0, // 控制商品类别
-    isAuction: 0, // 拍卖列表与售卖列表切换，0 - 拍卖列表 ，1 - 售卖列表
+    isAuction: 1, // 拍卖列表与售卖列表切换，0 - 拍卖列表 ，1 - 售卖列表
+    isBuy: 0, // 立即购买与即将开拍切换，0 - 立即购买 ，1 - 即将开拍
     dataList: [{
         goods_name: '全部',
         isActive: true,
@@ -15,7 +27,8 @@ Page({
         goods_img: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
         goods_xiaoliang: '限量抢购',
         goods_price: '60', //打折后
-        goods_original_price: '100' //原价
+        goods_original_price: '100', //原价
+        img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/ysc.png',
       },
       {
         goods_name: '全部',
@@ -28,8 +41,8 @@ Page({
         goods_img: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
         goods_xiaoliang: '限量抢购',
         goods_price: '70',
-        goods_original_price: '100' //原价
-
+        goods_original_price: '100', //原价
+        img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/ysc.png',
       }, {
         goods_name: '全部',
         isActive: false,
@@ -41,7 +54,8 @@ Page({
 
         goods_img: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
         goods_xiaoliang: '限量抢购',
-        goods_price: '80'
+        goods_price: '80',
+        img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/ysc.png',
       }, {
         goods_name: '全部',
         isActive: false,
@@ -53,7 +67,8 @@ Page({
           ,
         goods_img: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
         goods_xiaoliang: '限量抢购',
-        goods_price: '90'
+        goods_price: '90',
+        img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/ysc.png',
       }, {
         goods_name: '全部',
         isActive: false,
@@ -65,7 +80,8 @@ Page({
           ,
         goods_img: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
         goods_xiaoliang: '限量抢购',
-        goods_price: '110'
+        goods_price: '110',
+        img:'https://jgl.oss-cn-beijing.aliyuncs.com/sale/ysc.png',
       }
     ],
   },
