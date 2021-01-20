7a5aac994304d5d13684128de459d791a8b59d30
wenmengqi <574986464@qq.com>
2021-01-20 19:02:10 +0800

    首页数据测试、详情页数据测试

--- pages/auctionDetails/auctionDetails.js
+++ pages/auctionDetails/auctionDetails.js
@@ -5,88 +5,113 @@ Page({
    * 页面的初始数据
    */
   data: {
-    list: {
-      title: "铃木gsx250sr高配100多公里",
-      apply_num: 22,
-      circusee_time: 10000,
-      price: "2,222",
-      auction_info: {
-        auction_type: "增加拍",
-        auction_starting_price: 100,
-        auction_earnest_money: 50,
-        auction_amount: 300,
-        auction_start: "2020-12-11 13:17",
-        auction_over: "2020-12-12 13:17",
-        auction_reserve_price: "无",
-        auction_delayed: 5
-      },
-    },
+    // 商品数据
+    list: {},
+    // 出价记录
+    bidRecord: [],
+    // 店主信息
+    userInfo: {},
     // table栏选中
     isTable: 0,
-    // 情况
-    // 1 拍卖
-    // 2 售卖
-    auction_case: 1
+    // 商品类型： 0 拍卖商品 ， 1 售卖商品
+    auctionOrSale: '',
+    // 商品id
+    productId: '',
+    openid: '',
+    // 普通时间格式
+    timeList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad(options) {
+    // console.log(options)
+    const {auctionOrSale, productId, openid} = options
+    // 动态修改页面导航栏标题
+    wx.setNavigationBarTitle({ title: auctionOrSale == 0? '拍卖详情': '售卖详情' })      // options.name表示上个页面传过来的文字
+    this.setData({
+      auctionOrSale: auctionOrSale,
+      productId: productId,
+      openid: openid
+    })
+    // 发送数据请求
+    this.getData()
+    // 发送出价记录请求
+    this.getListId()
+    this.getUser()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 请求商品数据
+  getData() {
+    wx.request({
+      url: 'http://192.168.3.70:10010/jgl/product/jglProduct/selectProductDetail',
+      data: {
+        "auctionOrSale": this.data.auctionOrSale,
+        "productId": this.data.productId
+      },
+      method: 'POST',
+      success: (res) => {
+        // console.log(res)
+        const {data} = res.data
 
+        let TimeList = [data.startTime, data.endTime];
+        let TimeList2 = TimeList.map(x => {
+          // 将数据中的结束时间格式转化为 普通时间格式 - 便于后续倒计时把时间格式直接转化为 毫秒 
+          return this.renderTime(x).slice(0,16)
+        })
+
+        this.setData({
+          list: data,
+          timeList: TimeList2
+        })
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 出价记录数据请求
+  getListId() {
+    wx.request({
+      url: 'http://192.168.3.70:10010/jgl/bep/jglBid/selectListById',
+      data: {
+        "query": "4"
+      },
+      method: 'POST',
+      success: (res)=> {
+        // console.log(res)
+        const {records} = res.data.data
+        this.setData({bidRecord: records})
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 店主信息数据请求
+  getUser() {
+    wx.request({
+      url: 'http://192.168.3.70:10010/jgl/product/jglProduct/selectProductUser',
+      data: {
+        "auctionOrSale": this.data.isTable,
+        "openid": this.data.openid,
+        "productId": this.data.productId
+      },
+      method: 'POST',
+      success: (res)=>{
+        console.log(res)
+        const {data} = res.data
+        this.setData({userInfo: data})
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  // 时间格式转换 - 转换成 2021-xx-xx xx:xx:xx
+  renderTime(x) {
+    var dateee = new Date(x).toJSON();
+    return new Date(+new Date(dateee) + 0 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '')
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
+  // 切换拍卖宝贝和售卖宝贝栏
   handleClick(e) {
-    console.log(e.currentTarget.dataset.index)
     this.setData({isTable: e.currentTarget.dataset.index})
+    this.getUser()
   }
 })
