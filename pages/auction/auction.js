6e3f5ac5482141d4b233c41d7492dc62915a239c
王国君 <>
2021-01-25 13:32:34 +0800

    参与拍卖的切换

--- pages/auction/auction.js
+++ pages/auction/auction.js
@@ -1,8 +1,14 @@
 
 Page({
     data:{
-
+        show:0,    
+       
     },
+    handlePailistType(e) {
+        this.setData({
+            show: e.currentTarget.dataset.index
+        })
+      },
     onLoad: function (){
 
     },
