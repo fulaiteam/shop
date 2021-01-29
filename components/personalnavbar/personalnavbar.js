11c87eaf9be5fdf52c73481e6b0d6cda31f48c93
wenmengqi <574986464@qq.com>
2021-01-29 18:24:36 +0800

    测试

---
+++ components/personalnavbar/personalnavbar.js
@@ -0,0 +1,51 @@
+
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  externalClasses: ['custom-class'],
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    pageName:String,
+    showNav: {
+      type: Boolean,
+      value: true
+    },
+    bgColor:{
+      type: String,
+      value: '#fff'
+    },
+    iconColor:{
+      type: String,
+      value: '#000'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+   
+  },
+  lifetimes: {
+    attached: function () {
+      this.setData({
+        navHeight: getApp().globalData.navHeight,
+        navTop: getApp().globalData.navTop
+      })
+     }
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    //回退
+    _navBack: function () {
+      wx.navigateBack({
+        delta: 1
+      })      
+    }
+  }
+})
---
+++ pages/user_info/user_info.js
@@ -0,0 +1,58 @@
+// pages/user_info/user_info.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    phone: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({phone: options.userphone})
+  },
+
+  getUserInfo(e) {
+    console.log(e)
+    if (e.detail.errMsg == 'getUserInfo:ok') {
+      wx.login({
+        success: res => {
+          console.log("code转换", res.code); //用code传给服务器调换session_key
+          wx.request({
+            // url:getApp().globalData.baseUrl + '/wxlogin',
+            // url:'http://192.168.3.70:10010/jgl/user/jglUser/wxlogin',
+            url: getApp().globalData.baseUrl + 'user/jglUser/wxlogin',
+            method: 'post',
+            data: {
+              code: res.code,
+              encryptedData: e.detail.encryptedData,
+              iv: e.detail.iv,
+              phone: this.data.phone,
+              openid: '',
+              pcode: '',
+              sessionKey: ''
+            },
+            success: res => {
+              if (res.data.flag) {
+                getApp().globalData.openid = res.data.data.openid;
+                getApp().globalData.code = res.data.data.code;
+                console.log(getApp().globalData.openid, getApp().globalData.code, 2222)
+                wx.setStorageSync("code", res.data.data.code);
+                wx.setStorageSync("openid", res.data.data.openid);
+                wx.setStorageSync("session_key", res.data.data.sessionKey);
+                wx.switchTab({
+                  url: '/pages/personal/personal'
+                })
+              }
+            }
+          });
+        }
+      });
+    } else {
+      console.log('用户拒绝授权')
+    }
+  }
+})
